0de5f605159ca2a8e585e15542bab0bbd5fe0d2d
caetano <cbperuzzo4@gmail.com>
2024-06-03 16:01:20 -0300

    teste do git

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("oi davi")
+curuser = ""  #não poder usuário com nome vazio
+admin = False
+
+usuarios = list()
+
+def index():
+    while True:
+        print("registrar [reg] - login [log]")
+        res = input().upper().strip()
+
+        if(res == "REG"):
+            register()
+        elif(res == "LOG"):
+            log()
+        else:
+            "ação não encontrada, tente novamente"
+
+def log():
+    find = False
+    while True:
+        nome = input("nome:").upper().strip()
+        senha = input("senha:")
+
+        for i in range(0,len(usuarios),1):
+            if nome == (usuarios[i].nome).upper().strip():
+                if senha == usuarios[i].senha:
+                    curuser = usuarios[i].nome
+                    admin = usuarios[i].admin
+                    find = True
+        if find:
+            break
+        else:
+            print("usuário ou senha inválido")
+index()
+
