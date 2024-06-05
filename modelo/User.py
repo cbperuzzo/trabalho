235731ea132eeae8c6ab0795847d9ab26309e61e
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-06-05 16:18:05 -0300

    criei função para registrar user

--- modelo/User.py
+++ modelo/User.py
@@ -1,11 +1,40 @@
+import json
+from typing import List, Dict
+
 class User:
     def __init__(self, nome: str, senha: str, admin: bool = False):
-        this.nome = nome
-        this.senha = senha
-        this.admin = admin
+        if senha == '':
+            raise ValueError('Senha vazia.')
+        
+        self.nome = nome
+        self.senha = senha
+        self.admin = admin
+        
 
-    def save(self):
-        pass
+    def save(self) -> None:
+        user_login = {
+            'username': self.nome,
+            'password': self.senha,
+            'admin': False
+        }
+
+        with open('data_base/usuarios.json', 'r') as read_file:
+            all_users = json.load(read_file)
+        
+        if self.user_in_list(all_users, user_login):
+            raise ValueError('Nome de usuário já está sendo usado.')
+        
+        all_users.append(user_login)
+        
+        with open('data_base/usuarios.json', 'w') as write_file:
+            json.dump(all_users, write_file)
+
+    def user_in_list(self, user_list: List[Dict[str, str]], user_login) -> bool:
+        for registered_user in user_list:
+            if registered_user['username'] == user_login['username']:
+                return True
+        
+        return False
 
     @staticmethod
     def getAllUsers():
