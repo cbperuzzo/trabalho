86c2211b40d4d2347bde1240fb8dc6d9035a2c20
caetano <cbperuzzo4@gmail.com>
2024-06-05 17:22:06 -0300

    a

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 from controllers.userController import global_vars , indexUser
 
 indexUser()
 print('.',global_vars['curuser'], global_vars['admin'])
+=======
+from controllers.userController import indexUser
+
+indexUser()
+>>>>>>> a
