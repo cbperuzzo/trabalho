1122493a49dacbdbee1f2679077507a185610a72
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-06-05 16:43:43 -0300

    implementei verify login

--- modelo/User.py
+++ modelo/User.py
@@ -21,7 +21,7 @@ class User:
         with open('data_base/usuarios.json', 'r') as read_file:
             all_users = json.load(read_file)
         
-        if self.user_in_list(all_users, user_login):
+        if User.find_user_in_database(all_users, user_login['username']) != -1:
             raise ValueError('Nome de usuário já está sendo usado.')
         
         all_users.append(user_login)
@@ -29,17 +29,30 @@ class User:
         with open('data_base/usuarios.json', 'w') as write_file:
             json.dump(all_users, write_file)
 
-    def user_in_list(self, user_list: List[Dict[str, str]], user_login) -> bool:
-        for registered_user in user_list:
-            if registered_user['username'] == user_login['username']:
-                return True
+    @staticmethod
+    def find_user_in_database(username: str) -> int:
+        with open('data_base/usuarios.json', 'r') as read_file:
+            all_users = json.load(read_file)
         
-        return False
+        for registered_user in all_users:
+            if registered_user['username'] == user_login['username']:
+                return all_users.index(registered_user)
+        
+        return -1
 
     @staticmethod
-    def getAllUsers():
-        pass
+    def verify_login(username, password) -> None:
+        all_users = User.get_all_users()
 
+        user_index = User.find_user_in_database(username)
+        if user_index == -1:
+            raise ValueError('Nome de usuário não cadastrado.')
+        if all_users[user_index]['password'] != password:
+            raise ValueError('Senha incorreta.')
+        
     @staticmethod
-    def getWhereUserName(str):
-        pass
+    def get_all_users() -> List[Dict[str, str]]:
+        with open('data_base/usuarios.json', 'r') as read_file:
+            all_users = json.load(read_file)
+        
+        return all_users
